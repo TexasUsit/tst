1670735d1b4f432d7cccea099211c79b9a2c5996
dilan <hiradilan@gmail.com>
2017-03-03 09:20:30 -0600

    Minor content edits and added pictures of SAC, couldn't decide on which one so used both

--- scripts/register-form.js
+++ scripts/register-form.js
@@ -1,8 +1,27 @@
 $(document).ready(function(){
+	var i_image = 0;
 	$("#register-form").submit(function(e){
 		e.preventDefault();
 		$.post("https://script.google.com/macros/s/AKfycbzR0C-CuI1MZOeU0dhAVwgM6hfsx04fsjRRAQTFVxbCNZ0eAhw/exec",
 			  	$("#register-form").serialize(), function(data){});
 		window.location="http://127.0.0.1:58327/index.html";
 	});
+	
+	setInterval(function(){
+		if(i_image == 0){
+			$('#location-image').fadeOut(1000, function(){
+				$("#location-image").attr('src', 'images/sac-scene2.jpg');
+				$('#location-image').fadeIn(1000);
+			});
+		}
+		else{
+			$('#location-image').fadeOut(1000, function(){
+				$("#location-image").attr('src', 'images/sac-scene1.jpg');
+				$('#location-image').fadeIn(1000);
+			});
+		}
+		i_image++;
+		if(i_image > 1) {i_image=0;}
+	}, 5000);
+	
 });
