f6d40eeb45b81c214df58e1f3fcfa5de2983bd50
dilan <hiradilan@gmail.com>
2017-03-06 12:44:39 -0600

    Fixed register form, added kickoff, changed cover photo

--- scripts/register-form.js
+++ scripts/register-form.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 		e.preventDefault();
 		$.post("https://script.google.com/macros/s/AKfycbzR0C-CuI1MZOeU0dhAVwgM6hfsx04fsjRRAQTFVxbCNZ0eAhw/exec",
 			  	$("#register-form").serialize(), function(data){});
-		window.location="http://127.0.0.1:58327/index.html";
+		
 	});
 	
 	setInterval(function(){
